f681012c035b4a0225a9e991c4e5848ca7cfe17f
QuyAlc <quynguyen.sic@gmail.com>
2020-07-19 17:57:38 +0200

    I've edited inference file to generate the predict for new data file, that is X_needpredict fie

--- inference.py
+++ inference.py
@@ -1,38 +1,16 @@
 import os
 from joblib import load
 import numpy as np
+import pandas as pd
 
 # #############################################################################
 # Load model
 #print("Loading model from working directory")
 clf = load('CustomerClassification.joblib') 
 # #############################################################################
-# Run inference
-forecast = clf.forecast(steps=14)[0]
-result = [] 
-for i in forecast:
-    print(np.exp(i))
-    result.append(np.exp(i)*7)
-print("Predict revenue...")
-print(result)
-
-
-
-y_pred=clf.predict(X_test)
-
-
-X_NeedPredict=NeedPredict[['PRICE','FirstloginRange','Period','AveTrans','LAUNCH CAMPAIGN','MINOR EVENT SPONSORSHIP',
-               'MAJOR ADVERTISING BUY', 'MINOR ADVERTISING BUY','MAJOR EVENT SPONSORSHIP']]
+# Run inference	
+X_NeedPredict=pd.read_csv("data/X_NeedPredict.csv")
 
 y_pred_problem2=clf.predict(X_NeedPredict)
-
-
-NeedPredict['result']=y_pred_problem2
-
-Result_NeedPredict=NeedPredict[['USERID','result']]
-Result_NeedPredict.head()
-
-Result= Result_NeedPredict.append(Result_DontPredict, ignore_index=True)
-Result=Result.sort_values(by=['USERID'], ascending=True)
-
-
+print("The predict is: ")
+print(y_pred_problem2)
